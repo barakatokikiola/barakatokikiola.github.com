03739f38e867aa998e199eff2b4a64722a77aa5d
barakatokikiola <59037703+barakatokikiola@users.noreply.github.com>
2020-05-21 12:20:13 +0100

    change

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
 alert("Welcome, thanks for visiting");
-
-function ask(question,yes,no) {
-    if(confirm(question)) yes();
-   else no();
-}
-ask(
-    "Would you like to continue to this blog?",
-()=> alert ("Thanks for staying with us"),
-()=> alert("Thanks for visiting")
-);
